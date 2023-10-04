8b67a4fdf5f70b2a26f7c122156354ad8fe8f58f
clutch <lfcolchado@gmail.com>
2023-10-03 20:55:38 -0600

    Resize start y pause

--- Scripts/StartMenu.gd
+++ Scripts/StartMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var start_b = $VBoxContainer/VBoxContainer/start_button
 @onready var quit_b = $VBoxContainer/VBoxContainer/quit_button
-var next_scene_resource = preload("res://Scenes/MainWorld.tscn")
+var next_scene_resource = preload("res://Scenes/Gameplay.tscn")
 
 
 func _on_start_button_pressed():	
