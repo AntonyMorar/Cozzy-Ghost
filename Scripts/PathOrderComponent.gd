6ee1aad6d5e8e3b0ff08589780e3942ae159daa7
clutch <lfcolchado@gmail.com>
2023-10-06 17:01:53 -0600

    cambios en paths

--- Scripts/PathOrderComponent.gd
+++ Scripts/PathOrderComponent.gd
@@ -3,6 +3,7 @@ extends Node
 @export var path_list: Node3D
 var ordered_list_of_paths
 @export var character: Node3D
+@export var speed_anim:=1.0
 @export var animation: AnimationPlayer
 @export var pause_spook_time = 0.5
 var anim_num = 0
@@ -10,9 +11,11 @@ var anim_num = 0
 func _ready():
 	animation.connect('animation_finished', _anim_finished)
 	animation.play("path_"+str(anim_num))
+	animation.speed_scale = speed_anim
 	Events.connect('spookeado', _pausa_movimiento)
 	ordered_list_of_paths = path_list.get_children()
 	for path in ordered_list_of_paths:
+		path.get_child(0).get_child(0).remote_path = character.get_path()
 		path.get_child(0).rotation_mode = 1
 		path.get_child(0).loop = false
 		path.get_child(0).use_model_front = true
@@ -20,13 +23,13 @@ func _ready():
 
 func _anim_finished(anim_name):
 	var anim_name_num = anim_name.trim_suffix('path_').to_int()
+	animation.speed_scale = randf_range(0.3,0.7)
 	match  anim_name_num:
 		0:
-#			anim_num = randi_range(1,2)
 			anim_num = 1
 			animation.play("path_"+str(anim_num))
 		1:
-			anim_num = randi_range(2,3)
+			anim_num = [2,3,7].pick_random()
 			animation.play("path_"+str(anim_num))
 		2:
 			anim_num = [4,5].pick_random()
@@ -38,6 +41,15 @@ func _anim_finished(anim_name):
 			anim_num = 0
 			animation.play("path_"+str(anim_num))
 		5:
+			anim_num = 0
+			animation.play("path_"+str(anim_num))
+		6:
+			anim_num = 2
+			animation.play("path_"+str(anim_num))
+		7:
+			anim_num = 8
+			animation.play("path_"+str(anim_num))
+		8:
 			anim_num = 0
 			animation.play("path_"+str(anim_num))
 
