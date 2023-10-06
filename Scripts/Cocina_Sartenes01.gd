e4e976979f0b03468b04595a94fb604df903db58
Antony M <antony999k@hotmail.com>
2023-10-06 14:26:54 -0600

    :sparkles: minior scene update

--- Scripts/Cocina_Sartenes01.gd
+++ Scripts/Cocina_Sartenes01.gd
@@ -1,12 +1,14 @@
 extends MeshInstance3D
 
 var is_active = false
+@export var animation:AnimationPlayer
 
 func _clicked():
+	print("Click sarten")
 	if !is_active:
 		is_active = true
-		#Globals.ya_clickeo_sarten = true
-		$AnimationPlayer.play("agitar_sartenes")
+		Globals.ya_clickeo_sarten = true
+		animation.play("agitar_sartenes")
 		
 func _active_again():
 	is_active = false
