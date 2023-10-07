1014e4b5b3602c8806738ef513d94436f2b38990
Antony M <antony999k@hotmail.com>
2023-10-06 18:22:43 -0600

    :sparkles: Fixed objects

--- Scripts/Objects/BaseObject.gd
+++ Scripts/Objects/BaseObject.gd
@@ -1,31 +1,42 @@
 extends MeshInstance3D
 
-var is_active = false
+@export_group("Parametros")
+@export var autolock:bool = true #True if use cooldown to unlock
+@export var cooldown: float = 0.15
 
-@export var autolock:bool = true
-
+@export_group("Acciones")
 @export var animation:AnimationPlayer
 @export var light:Light3D
 @export var effect:GPUParticles3D
 
+var is_active = false
+var actualCooldown;
+
+func _ready():
+	actualCooldown = cooldown
+
+func _process(_delta):
+	if is_active and autolock:
+		actualCooldown -= _delta
+		if(actualCooldown <= 0):
+			is_active = false
+			actualCooldown = cooldown
+
 func _clicked():
+	if is_active: return
 	print("Clicked")
-	if is_active and autolock: return
-	if autolock:
-		is_active = true
-	else:
-		is_active = !is_active
+	is_active = true
 	#Animation
 	if animation: animation.play("Move")
 	#Ligth
-	if light: 
-		if is_active:
-			light.light_energy = 0
-		else:
+	if light:
+		if light.light_energy == 0:
 			light.light_energy = 3
+		else:
+			light.light_energy = 0
 	#VFX
 	if effect:
 		effect.emitting = true
 		
-func _active():
+func _deactive():
 	is_active = false
